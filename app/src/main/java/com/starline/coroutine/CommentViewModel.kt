b87107860f361d7343dbbb043143fddff29617a8
jianyu.yang <jianyu.yang@mobvoi.com>
2022-05-10 14:31:19 +0800

    Update CommentViewModel.kt

--- app/src/main/java/com/starline/coroutine/CommentViewModel.kt
+++ app/src/main/java/com/starline/coroutine/CommentViewModel.kt
@@ -29,62 +29,25 @@ class CommentViewModel : ViewModel() {
     val commentFlow = MutableStateFlow<ApiState<CommentModel>>(ApiState.loading())
 
 
-    fun getComment(postId: Int) {
-        //Network call will takes time, so we set the value to loading state
-        commentFlow.value = ApiState.loading()
-        //Network call will takes time, so has to be run in the background thread.
-        //use viewModelScope to limit the call available only when viewModel still alive.
-        viewModelScope.launch {
-            try {
-                //if api call is succeeded, set the state to success, and set data to data received from api
-                commentFlow.value = client.getComment(postId)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                //ignore CancellationException
-                if (e is CancellationException) {
-                    return@launch
-                }
-                //If any error occurs like 404 or unknown host, it will be catch in here, and we set the state to error, so the ui will show some info
-                commentFlow.value = ApiState.error(e.message.toString())
-            }
-        }
-    }
-
-    fun getUser() {
-        //Network call will takes time, so we set the value to loading state
-        usersFlow.value = ApiState.loading()
-        //Network call will takes time, so has to be run in the background thread.
-        //use viewModelScope to limit the call available only when viewModel still alive.
-        viewModelScope.launch {
-            try {
-                //if api call is succeeded, set the state to success, and set data to data received from api
-                usersFlow.value = client.getUser()
-            } catch (e: Exception) {
-                e.printStackTrace()
-                //ignore CancellationException
-                if (e is CancellationException) {
-                    return@launch
-                }
-                //If any error occurs like 404 or unknown host, it will be catch in here, and we set the state to error, so the ui will show some info
-                usersFlow.value = ApiState.error(e.message.toString())
-            }
-        }
-    }
-
     fun serialRequest() {
+        //reset ui to loading status
         usersFlow.value = ApiState.loading()
         commentFlow.value = ApiState.loading()
+
         viewModelScope.launch {
             val user = client.getUser()
             usersFlow.emit(user)
+
             val comment = client.getComment(user.data?.id!!)
             commentFlow.emit(comment)
         }
     }
 
     fun parallelRequest() {
+        //reset ui to loading status
         usersFlow.value = ApiState.loading()
         commentFlow.value = ApiState.loading()
+
         viewModelScope.launch {
             usersFlow.emit(client.getUser())
         }
@@ -94,8 +57,10 @@ class CommentViewModel : ViewModel() {
     }
 
     fun parallelMergeRequest() {
+        //reset ui to loading status
         usersFlow.value = ApiState.loading()
         commentFlow.value = ApiState.loading()
+
         viewModelScope.launch {
             //get user
             val user = async {
@@ -105,6 +70,8 @@ class CommentViewModel : ViewModel() {
             val comment = async {
                 client.getComment(1)
             }
+
+            //execute when user and comment request are both finished
             merge(user.await(), comment.await())
         }
     }
